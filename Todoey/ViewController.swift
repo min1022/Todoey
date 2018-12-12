ef7bc8336351eadcf7044e7afea592945c16b3a1
songkyung min <min.songkyung@gmail.com>
2018-12-12 17:33:19 +0900

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by GlobalHumanism on 2018. 12. 12..
+//  Copyright © 2018년 GlobalHumanism. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
